2e08775f5142df8239d796f8972108e1ce03e930
Antonio <antonioroic@gmail.com>
2018-09-11 13:10:45 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import cs from 'classnames';
+import './TextInput.css';
+import './RandomColor.css';
+
+
+function TextInput(props) {
+  return <input
+    className={cs(['input', props.className])}
+    type={props.type}
+    onChange={props.onChange}
+    placeholder={props.placeholder}
+    value={props.value}
+  />
+}
+
+
+function RandomColor(props) {
+  return <div
+    className={cs(['center', props.className])}
+    style={{backgroundColor: props.color}}
+    onClick={props.onClick}
+  >
+    {'Klick me!'}
+  </div>
+}
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={ 
+    text: '', 
+    color: 'black',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  
+  handleChange(event) {
+    this.setState({text: event.target.value});
+  }
+  
+
   render() {
     return (
       <div className="App">
@@ -10,9 +53,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <RandomColor 
+         onClick={this.onChangeColor} 
+         color={this.state.color} 
+        />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        {this.state.text}
         </p>
+        <TextInput
+          type={'text'}
+          name='text'
+          onChange={this.handleChange}
+          placeholder={'Write something!'}
+          value={this.state.text}
+        />
       </div>
     );
   }
